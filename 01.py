9ca0af2f5b5c5b8d5cf006a83d58f9acec7b17d1
Maryna Sibova <maryna.i.kipenko@gmail.com>
2025-08-15 02:08:55 -0400

    factory pattern and SOLID

---
+++ 01.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+class Car(Vehicle):
+    def __init__(self, make, model, region):
+        self.make = make
+        self.model = model
+        self.region = region
+
+    def start_engine(self):
+        print(f"{self.make} {self.model} ({self.region} Spec): Двигун запущено")
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, region):
+        self.make = make
+        self.model = model
+        self.region = region
+
+    def start_engine(self):
+        print(f"{self.make} {self.model} ({self.region} Spec): Мотор заведено")
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car():
+        pass
+
+    @abstractmethod
+    def create_motorcycle():
+        pass
+
+class USVehicleFactory(VehicleFactory):
+    def create_car(self, make, model) -> Vehicle:
+        return Car(make, model, "US")
+
+    def create_motorcycle(self, make, model) -> Vehicle:
+        return Motorcycle(make, model, "US")
+
+
+class EUVehicleFactory(VehicleFactory):
+    def create_car(self, make, model) -> Vehicle:
+        return Car(make, model, "EU")
+
+    def create_motorcycle(self, make, model) -> Vehicle:
+        return Motorcycle(make, model, "EU")
+
+
+# Використання
+us_factory = USVehicleFactory()
+eu_factory = EUVehicleFactory()
+
+vehicle1 = us_factory.create_car("Toyota", "Corolla")
+vehicle1.start_engine()
+
+vehicle2 = eu_factory.create_motorcycle("Harley-Davidson", "Sportster")
+vehicle2.start_engine()
